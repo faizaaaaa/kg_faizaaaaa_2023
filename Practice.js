7e7c9edd7be318bedb11cf87a9e87be7f4129b12
faizaaaaa <77255372+faizaaaaa@users.noreply.github.com>
2021-04-27 21:15:37 -0400

    Add files via upload
    Mechanism to parse every digit of an integer into an array of those digits, separated by a comma. Currently, the program does not accept user input but rather parses values hardcoded in the program, in the variable defined as 'number'.

---
+++ Practice.js
@@ -0,0 +1,9 @@
+var array = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
+var number = 3485893
+var arr = (String(number)
+        .split('')
+        .map(Number));
+    
+console.log(arr);
+
+
