b7ec6f48a1489ab429307fe33394bbf8bc4bfb64
faizaaaaa <77255372+faizaaaaa@users.noreply.github.com>
2021-04-27 21:42:02 -0400

    Add files via upload
    Updated to accept and parse command line input, through the implementation of two functions: the converterFunction which parses each command-line argument, and the conversionFunction which associates each number to its corresponding word value. This update converts each input into user expected output, however, each number is printed out separately.

--- Practice.js
+++ Practice.js
@@ -1,9 +1,17 @@
 var array = ['One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten'];
-var number = 3485893
-var arr = (String(number)
+
+((process.argv).slice(2)).forEach(converterFunction);
+
+
+function converterFunction(indexTwo, ind, arrays) {
+    Strings = "";
+    var arr = (String(indexTwo)
         .split('')
-        .map(Number));
-    
-console.log(arr);
+        .map(Number)).forEach(conversionFunction);
+    arrays[ind] = Strings;
+    console.log(Strings);
+}
 
-
+function conversionFunction(index) {
+    Strings += array[index - 1];
+}
